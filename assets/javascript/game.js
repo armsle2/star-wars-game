77d197d7779023697a0db77a81633c032714f79a
LA The Fly Rebel <flyrebel@server.theflyrebel.com>
2017-11-24 14:52:02 -0500

    figured out to use jquery show/hide method. styled css to have all characters listed at all levels.

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+//on load show character options
+	//each character should have their name and HP displayed in their boxes
+	//wait for player to choose a character
+//when a player chooses a character
+	//remove the remaining characters to the enemies to attack section
+	//wait for player to choose enemy to fight
+//when player chooses enemy to fight
+	//move enemy to defender section
+	//wait for for player to push fight button
+//when player clicks fight button
+	// message displays the amount of each attack
+	//player's attack power reduces defender's hp
+	//defender's counter attack power reduces player's hp
+	//player's attack power is added by its base each time attack button is clicked
+	//counter attack power does not change
+//if player loses
+	//message displays Game Over
+	//button will appear to restart game
+//if player wins
+	//defender disappears
+	//message displays you have defeated enemy and now you may choose to fight another enemy
+	//players hp and attack power does NOT reset
+//if attack button is clicked before player chooses an enemey
+	//message displays no enemies chosen
+//if all enemies have been defeated
+	//display message You Won Game Over
+
+$(document).ready(function(){
+
+$("#enemy").hide("enemy-1");
+
+// $("#player-2").click(function(){
+// 	$("#enemy").append(luke);
+// 	$("#player-1").hide(luke);
+
+
+
+
+});
